00d546175e73cfc495434349d819c43f7aabfc11
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-24 20:38:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, redirect
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return redirect("https://trustedge.com/", code = 302)
+def index():
+    return render_template('index.html', code = 302)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
