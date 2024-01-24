d3438911b8bea5862675004283e11c9c4f60c7ae
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-24 21:18:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return 'Welcome to the Flask App!'
-
 @app.route('/redirect')
 def redirect_to_main_website():
     # Replace 'https://example.com' with the actual URL of your main website
-    time.sleep(60)  # Sleep for 60 seconds
-    return redirect('https://trustedge.com')
+    time.sleep(5)  # Sleep for 60 seconds
+    return redirect("https://trustedge.com/", code = 302)
 
 if __name__ == '__main__':
     app.run(debug=True)
