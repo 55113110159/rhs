10906aac40646c166e210a9ee07b45a5dfa8da87
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-24 21:34:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import time
 from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    time.sleep(60)  # Sleep for 60 seconds
     return redirect("https://trustedge.com/", code = 302)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
